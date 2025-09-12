8413511d2d082ca18aed000cb44079adf06b0db9
pooja <poojanairjuly19@gmail.com>
2025-09-12 22:36:04 +0530

    resolved merge conflict

--- src/main/java/com/pooju/quizlet/controller/QuizController.java
+++ src/main/java/com/pooju/quizlet/controller/QuizController.java
@@ -4,10 +4,7 @@ import com.pooju.quizlet.dto.Request;
 import com.pooju.quizlet.model.Quiz;
 import com.pooju.quizlet.service.QuizService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
